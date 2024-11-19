5bd20527f8bb2746b06550f27478578c1d94fae8
Stan-tab <godofwarmafia2@gmail.com>
2024-11-19 19:54:19 +0500

    Finished third assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(reversed) {
+    arrayer = []
+    for (i of reversed) {
+        arrayer.unshift(i)
+    }
+    
+    return arrayer.join("")
 };
 
 // Do not edit below this line
