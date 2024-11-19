105b6c82ae3c6dc5a8bf54654daf23955fdf62cd
Stan-tab <godofwarmafia2@gmail.com>
2024-11-19 19:39:38 +0500

    Finished second assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(words, repeats) {
+    let sentense = '';
+    if (repeats<0) {return "ERROR"}
+    for (let i=1; i<=repeats; i++){
+        sentense += words;
+    }
+    return sentense
 };
 
 // Do not edit below this line
