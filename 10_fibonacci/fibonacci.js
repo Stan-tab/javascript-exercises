a6f56274c1209813bce1c5531821f8082e77dff9
Stan-tab <godofwarmafia2@gmail.com>
2024-12-01 16:03:27 +0500

    Finished 10-th exercice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (+number == 0) {return 0}
+    if (+number < 0 || !Number.isInteger(+number)) {return "OOPS"}
+    let arr = [1, ];
+    for (let i = 2; i <= +number; i++) {
+        arr.push(+arr.slice(-2, -1) + +arr.slice(-1));
+    }
+    return +arr.slice(-1);
 };
 
 // Do not edit below this line
