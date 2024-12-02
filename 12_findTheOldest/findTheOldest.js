bea8bb3732bb6acaf112234e7113d3f3a0d51ec6
Stan-tab <godofwarmafia2@gmail.com>
2024-12-02 21:55:51 +0500

    Finished 12-th exercice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    let data = new Date();
+    const year = data.getFullYear();
+    list.forEach(current => {
+        if (!isNaN(current.yearOfDeath)) {current.liveTime = current.yearOfDeath - current.yearOfBirth;}
+        else{current.liveTime = year - current.yearOfBirth;}
+    });
+    num = list.reduce((ac, item) => {
+        if (ac >=  item.liveTime) {
+            return ac;
+        } else {
+            return item.liveTime;
+        }
+    }, 1)
+    return list.find((item) => item.liveTime == num);
 };
 
 // Do not edit below this line
